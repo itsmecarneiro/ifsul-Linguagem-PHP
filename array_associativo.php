500781061a1f5389e9cbcee559ed2d999ee096b8
Jessica <eujeca@hotmail.com>
2026-03-28 20:04:59 -0300

    feat: adiciona exercício sobre array associativo

---
+++ array_associativo.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h3>Exercício 1<h3>";
+
+$produtoExercicio1 = [
+    "nome" => "Notebook",
+    "preco" => 3500,
+    "marca" => "Dell"
+];
+
+echo "Produto: " . $produto["preco"];
+?>
