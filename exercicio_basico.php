c2a194f10285ae69d5839774a3b835c957237d20
Jessica <eujeca@hotmail.com>
2026-04-12 09:32:04 -0300

    feat: adiciona exercícios usando array

---
+++ exercicio_basico.php
@@ -0,0 +1,9 @@
+<?php
+$alunos = [
+    "Jessica", "Helena", "Ana", "João", "Paulo"
+];
+
+foreach ($alunos as $aluno){
+    echo $aluno;
+}
+?>
---
+++ formulario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inscrição</title>
+</head>
+<body>
+    <h1>Banco de Talentos - Estágio em PHP</h1>
+<form name="cadastro" action="cadastro.php" method="post">
+    Nome: <input type="text" name="nome" />
+    <br>
+    Email: <input type="text" name="email" />
+    <br>
+    Cidade: <input type="text" name="cidade" />
+    <br>
+
+</form>
+</body>
+
+</html>
