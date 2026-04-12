e4f103c4f6da974b349db6e6b92a561ab486f37c
Jessica <eujeca@hotmail.com>
2026-04-12 15:39:35 -0300

    feat: adiciona meu primeiro exercício com formulário

--- formulario.php
+++ formulario.php
@@ -5,15 +5,31 @@
 </head>
 <body>
     <h1>Banco de Talentos - Estágio em PHP</h1>
-<form name="cadastro" action="cadastro.php" method="post">
+<form name="formulario" action="formulario.php" method="POST">
     Nome: <input type="text" name="nome" />
     <br>
     Email: <input type="text" name="email" />
     <br>
     Cidade: <input type="text" name="cidade" />
     <br>
-
+    <button type="submit" >Enviar</button>
 </form>
-</body>
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $cidade = $_POST["cidade"];
 
+    echo "<h3>Dados recebidos:</h3>";
+
+    if(empty($nome)){
+    echo "<p style='color:red;'>Nome é obrigatório!</p>";
+    } else{
+    echo "<p>Nome: $nome</p>";
+    echo "<p>Email: $email</p>";
+    echo "<p>Cidade: $cidade</p>";
+    }
+}
+?>
+</body>
 </html>
