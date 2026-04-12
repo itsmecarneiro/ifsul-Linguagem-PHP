370e560f767dddb15ab06aa222ecf3158c25fabd
Jessica <eujeca@hotmail.com>
2026-04-12 09:51:56 -0300

    feat: adiciona um exercício de array e foreach

--- exercicio_soma.php
+++ exercicio_soma.php
@@ -1,5 +1,19 @@
 <?php
+$notas = [
+    10, 5, 8, 6, 9
+];
 
+echo "<h3>Notas:</h3>";
 
+foreach($notas as $nota){
+    echo $nota . "<br>";
+}
 
+$total = 0;
+
+foreach($notas as $nota){
+    $total += $nota;
+}
+
+echo "<h3>Total: $total </h3>";
 ?>
